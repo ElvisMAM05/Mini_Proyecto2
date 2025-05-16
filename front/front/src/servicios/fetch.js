464f6edca2e730b94921137fae86e006c17d6ac7
ElvisMAM05 <eaguilar119500862@gmail.com>
2025-05-16 13:22:42 -0600

    Commit 3

---
+++ front/front/src/servicios/fetch.js
@@ -0,0 +1,65 @@
+async function postData(obj,endpoint) {
+    const peticion = await fetch(`http://127.0.0.1:8000/${endpoint}`,{
+        method: "POST",
+        headers:{
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify(obj)        
+    })
+    const res = peticion.json()
+    return res
+}
+export {postData}
+
+
+async function deleteData(id,endpoint) {
+    try {
+        const response = await fetch(`http://127.0.0.1:8000/${endpoint}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        if (!response.ok) {
+            throw new Error(`Error deleting user with id ${id}`);
+        }
+        
+        return { message: `User with id ${id} deleted successfully` };
+    } catch (error) {
+        console.error('Error deleting user:', error);
+        throw error;
+    }
+}
+export {deleteData}
+
+async function patchData(info,endpoint,id) {
+    try {
+        const peticion = await fetch(`http://127.0.0.1:8000/${endpoint}/${id}`,{
+          method:"PATCH",
+          headers:{
+            "Content-type":"application/json"
+          }, 
+          body: JSON.stringify(info)
+        })
+        const datos = await peticion.json()
+        console.log(datos);
+        return datos
+      } catch (error) {
+          console.error(error);
+      }
+}
+export {patchData}
+
+const getData=async(endpoint) => {
+    try {
+        const peticion = await fetch(`http://127.0.0.1:8000/${endpoint}`)
+        const datos = await peticion.json()
+        console.log(datos);
+        return datos
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export {getData}
